311c32e3cf83781cea17358d94a8aa440b1df22f
Girish <3790179+sgk1@users.noreply.github.com>
2025-12-30 21:57:33 +0530

    Create html and js files for rock-pap-scissor game
    This commit consists of adding two separate files index.html and script.js for taking no. of rounds as input and looping in rock-paper-scissors game.

---
+++ script.js
@@ -0,0 +1,97 @@
+// javascript code goes here
+
+
+function getComputerChoice() {
+    const a =Math.floor(Math.random()*100);
+    if (a<33) {
+        console.log("Computer choice is: rock")
+        return "rock";
+    } 
+    else if (a>=33 && a<66) {
+        console.log("Computer choice is: paper")
+        return "paper";
+    } 
+    else if (a<100) {
+        console.log("Computer choice is: scissors")
+        return "scissors";
+    }
+}
+
+
+function getHumanChoice() {
+    const input = prompt("Type your choice: rock, paper or scissors?");
+    console.log(`User input is: ${input}`);
+    const humanSelection = input.toLowerCase();
+    return humanSelection;
+}
+
+
+function playGame (noOfRoundsPlayed) {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i< rounds;i++) {
+    //getHumanChoice();
+    //getComputerChoice();
+    computerSelection = getComputerChoice();
+    humanSelection = getHumanChoice();
+    playRound(humanSelection,computerSelection);
+}
+
+console.log(`Final computerScore is ${computerScore}`);
+console.log(`Final humanScore is ${humanScore}`);
+console.log(computerScore>humanScore?"Computer wins overall!":"You win overall!")
+
+    function playRound (humanChoice,computerChoice)
+    {
+        if (humanChoice == "rock" && computerChoice =="paper" )
+        {
+            console.log ("You lose!Paper beats Rock.")
+            computerScore++;
+        } else if (humanChoice == "rock" && computerChoice =="scissors" )
+        {
+            console.log ("You win!Rock beats Scissors.")
+            humanScore++;
+        } else if (humanChoice == "paper" && computerChoice =="rock" )
+        {
+            console.log ("You win!Paper beats Rock.")
+            humanScore++;
+        } else if (humanChoice == "paper" && computerChoice =="scissors" )
+        {
+            console.log ("You lose!Scissors beats paper.")
+            computerScore++;
+        } else if (humanChoice == "scissors" && computerChoice =="rock" )
+        {
+            console.log ("You lose!Rock beats Scissors.")
+            computerScore++;
+        } else if (humanChoice == "scissors" && computerChoice =="paper" )
+        {
+            console.log ("You win!Scissors beats Paper.")
+            humanScore++;
+        } else {
+            console.log ("It is a draw!")
+        }
+    }
+
+
+    
+
+}
+
+
+let computerSelection;
+let humanSelection;
+
+
+function noOfRounds () {
+    const input = prompt ("How many rounds you want to play with computer?","Enter a number");
+    const rounds = Number(input);
+    return rounds;
+
+}
+
+const rounds = noOfRounds();
+
+playGame(rounds);
+
